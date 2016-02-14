5508274a1783168529e1a89f635c590cdeee0555
Stephan Rabanser <steverab93@gmail.com>
2016-02-14 17:12:16 -0500

    Integrate examplaring charts for requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,6 +3,17 @@ class RequestsController < ApplicationController
 
   def list
     @requests = Request.all
+
+    @countryCountMap = {}
+
+    all = @requests.clone.as_json
+    all.group_by{ |h| h['country'] }.each do |loc,items|
+      puts "#{loc}: #{items.length} request#{:s if items.length!=1}"
+      @countryCountMap[loc] = items.length
+      #print "--> "
+      #puts events.map{ |e| e['status'] }.join(', ')
+    end
+
     render :list
   end
 
