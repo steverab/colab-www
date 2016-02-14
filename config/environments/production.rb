62ffab8cd1580b3886ef52a5693a51dbeb5e4fba
Stephan Rabanser <steverab93@gmail.com>
2016-02-14 12:00:07 -0500

    Add API URL for environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.api_server_url = "https://colab-api.herokuapp.com"
 end
